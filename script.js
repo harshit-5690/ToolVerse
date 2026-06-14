c9525db25c505e4db13bff74ad7ccb405d8df9ca
harshit-5690 <is1202920@gmail.com>
2026-06-14 08:01:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ link:"https://chatgpt.com"
 },
 
 {
+name:"Gemini",
+image:"https://cdn.simpleicons.org/google",
+description:"Google AI",
+link:"https://gemini.google.com"
+},
+
+{
 name:"GitHub",
 image:"https://cdn.simpleicons.org/github",
 description:"Code Hosting",
@@ -19,6 +26,20 @@ name:"YouTube",
 image:"https://cdn.simpleicons.org/youtube",
 description:"Video Platform",
 link:"https://youtube.com"
+},
+
+{
+name:"Instagram",
+image:"https://cdn.simpleicons.org/instagram",
+description:"Social Media",
+link:"https://instagram.com"
+},
+
+{
+name:"Telegram",
+image:"https://cdn.simpleicons.org/telegram",
+description:"Messaging",
+link:"https://telegram.org"
 }
 
 ];
@@ -28,12 +49,11 @@ document.getElementById("appContainer");
 
 function renderApps(data){
 
-appContainer.innerHTML = "";
+appContainer.innerHTML="";
 
 data.forEach(app=>{
 
 appContainer.innerHTML += `
-
 <div class="app-card">
 
 <img src="${app.image}">
@@ -42,8 +62,7 @@ appContainer.innerHTML += `
 
 <p>${app.description}</p>
 
-<a
-href="${app.link}"
+<a href="${app.link}"
 target="_blank"
 class="download-btn">
 
@@ -52,7 +71,6 @@ Open
 </a>
 
 </div>
-
 `;
 
 });
@@ -61,13 +79,12 @@ Open
 
 renderApps(apps);
 
-const search =
-document.getElementById("search");
-
-search.addEventListener("input",()=>{
+document
+.getElementById("search")
+.addEventListener("input",e=>{
 
 const value =
-search.value.toLowerCase();
+e.target.value.toLowerCase();
 
 const filtered =
 apps.filter(app=>
@@ -77,7 +94,8 @@ app.name.toLowerCase()
 
 ||
 
-app.description.toLowerCase()
+app.description
+.toLowerCase()
 .includes(value)
 
 );
