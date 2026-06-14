aaf7abb40fde105c9d3fb83d8ff2c8d470bcad56
harshit-5690 <is1202920@gmail.com>
2026-06-14 07:27:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,119 @@
-console.log("ToolVerse Loaded");
+// ToolVerse Apps Data
+
+const apps = [
+
+{
+name:"ChatGPT",
+image:"IMG_20260611_215851_582.webp",
+description:"AI Assistant by OpenAI",
+link:"#"
+},
+
+{
+name:"Gemini",
+image:"IMG_20260611_215851_582.webp",
+description:"Google AI Assistant",
+link:"#"
+},
+
+{
+name:"ToolVerse",
+image:"IMG_20260611_215851_582.webp",
+description:"Apps, Tools & Downloads",
+link:"#"
+}
+
+];
+
+// Container
+
+const appContainer =
+document.getElementById("appContainer");
+
+// Create Cards
+
+function renderApps(data){
+
+appContainer.innerHTML="";
+
+data.forEach(app=>{
+
+appContainer.innerHTML += `
+
+<div class="app-card">
+
+<img src="${app.image}">
+
+<h3>${app.name}</h3>
+
+<p>${app.description}</p>
+
+<a
+href="${app.link}"
+class="download-btn">
+
+Open
+
+</a>
+
+</div>
+
+`;
+
+});
+
+}
+
+// Initial Load
+
+renderApps(apps);
+
+// Search
+
+const search =
+document.getElementById("search");
+
+search.addEventListener("input",()=>{
+
+const value =
+search.value.toLowerCase();
+
+const filtered =
+apps.filter(app=>
+
+app.name.toLowerCase()
+.includes(value)
+
+||
+
+app.description
+.toLowerCase()
+.includes(value)
+
+);
+
+renderApps(filtered);
+
+});
+
+// Future Function
+
+function addApp(
+name,
+image,
+description,
+link
+){
+
+apps.push({
+
+name,
+image,
+description,
+link
+
+});
+
+renderApps(apps);
+
+}
